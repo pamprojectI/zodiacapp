15b8e8e924895f403852d8419f53efbd9b80fc09
pamprojectI <138356093+pamprojectI@users.noreply.github.com>
2024-09-01 21:09:06 +0700

    Update NavMenu.vue

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -4,7 +4,7 @@
             <li><a class="active" href="./">Home</a></li>
             <li><a href="#news">News</a></li>
             <li><a href="#contact">Contact</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="#about">About Us</a></li>
         </ul>
     </div>
 </template>
